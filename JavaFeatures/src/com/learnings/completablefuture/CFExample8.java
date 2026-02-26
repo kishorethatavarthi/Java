680576088b3a8869a838b10d05eece3cc329f551
kishore <kishorlearnings@gmail.com>
2026-02-26 16:29:57 +0530

    CF Examples commit

--- JavaFeatures/src/com/learnings/completablefuture/CFExample8.java
+++ JavaFeatures/src/com/learnings/completablefuture/CFExample8.java
@@ -5,7 +5,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class CFExample3 {
+public class CFExample8 {
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
 		ExecutorService executor = Executors.newFixedThreadPool(2);
 		ExecutorService executor1 = Executors.newFixedThreadPool(3);
@@ -16,11 +16,11 @@ public class CFExample3 {
                         Thread.currentThread().getName());
             	return 5;
             }, executor)
-                .thenApplyAsync(x -> {
+                .thenApply(x -> {
                     System.out.println("Transform in " +
                         Thread.currentThread().getName());
                     return x * 2;
-                }, executor1);
+                });
 
         System.out.println(cf.get()); // 10
         executor.shutdown();
